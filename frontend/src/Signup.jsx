ea01e138c50df679f26f484f314b626336166490
ashu7982 <ashutoshdubey7982@gmail.com>
2023-07-17 14:50:51 +0530

    done

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -42,7 +42,7 @@ export const SignupPage = () => {
     const {name,email,gender,password}=state
     const handlesubmit=(e)=>{
      e.preventDefault()
-    axios.post("https://vast-teal-tuna-cap.cyclic.app/users/register",state)
+    axios.post("https://bright-deer-onesies.cyclic.app/users/register",state)
     .then((res)=>{
         console.log(res)
     })
@@ -61,6 +61,36 @@ export const SignupPage = () => {
            Gender:- <input type="text"  onChange={(e)=>dispatch({type:"Gender",payload:e.target.value})} name='gender' value={gender} placeholder='Gender' /> <br />
            <input type="submit" /> 
         </form>
+    </div>
+  )
+}
+
+import axios from 'axios'
+import React, { useState } from 'react'
+
+export default function Signup() {
+ let [email,setEmail]=useState('')
+ let [name,setname]=useState('')
+ let [gender,setgender]=useState('')
+ let [password,setpass]=useState('')
+
+
+ function handleSubmit(){
+    axios.post('https://bright-deer-onesies.cyclic.app/users/register',{
+        body:{
+            email,name,gender,password
+        }
+    })
+    .then((res)=>{alert(res)})
+    .catch((err)=>alert(err))
+ }
+  return (
+    <div style={{display:'flex',flexDirection:'column',margin:'auto',width:'50%'}}>
+       <input type="text"  placeholder='name' onChange={(e)=>setname(e.value)}/>
+       <input type="email" placeholder='email'onChange={(e)=>setEmail(e.value)}/>
+       <input type="text" placeholder='gender' onChange={(e)=>setgender(e.value)}/>
+       <input type="password" placeholder='password' onChange={(e)=>setpass(e.value)}/>
+       <button onClick={(e)=>handleSubmit()}>submit</button>
     </div>
   )
 }
--- frontend/src/component/PostPage.jsx
+++
@@ -1,9 +0,0 @@
-
-
-import React from 'react'
-
-export const PostPage = () => {
-  return (
-    <div>PostPage</div>
-  )
-}
