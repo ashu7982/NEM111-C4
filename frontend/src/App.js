9b546e2fc409186ef0c32af4a4751ae4292fc41f
ashu7982 <ashutoshdubey7982@gmail.com>
2023-07-17 14:57:01 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { AllRoutes } from './AllRoutes';
+import Navbar from './navbar';
+import MainRoute from './mainRoute';
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+      <Navbar/>
+      <MainRoute/>
     </div>
   );
 }
