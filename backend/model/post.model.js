384e205dcc7f24285e61f566aa25198c7c44d621
ashu7982 <ashutoshdubey7982@gmail.com>
2023-07-17 12:12:02 +0530

    done

---
+++ backend/model/post.model.js
@@ -0,0 +1,15 @@
+
+
+const { Schema, model } = require("mongoose");
+
+const postSchema=Schema({
+    title:String,
+    body:String,
+    device:String,
+    userId:String,
+    username:String
+})
+
+// this is model
+const PostModel=model("Post",postSchema)
+module.exports={PostModel}
---
+++ backend/model/user.model.js
@@ -0,0 +1,16 @@
+
+
+const { Schema, model } = require("mongoose");
+
+const userSchema=Schema({
+    name:String,
+    email:String,
+    gender:String,
+    password:String
+})
+
+// this is user model
+
+const UserModel=model("user",userSchema)
+
+module.exports={UserModel}
