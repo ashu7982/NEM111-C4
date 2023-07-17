3a730c509cccb42860bab8cd09bc29a148f1a638
ashu7982 <ashutoshdubey7982@gmail.com>
2023-07-17 13:57:55 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,61 @@
-const express=require('express');
+
+
+// const express=require("express");
+// const { connection } = require("./db");
+// const { userRouter } = require("./Routes/user.routes");
+// const { PostRouter } = require("./Routes/post.routes");
+// const cors = require('cors');
+// require("dotenv").config();
+
+
+// const app=express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/users",userRouter);
+// app.use("/posts",PostRouter);
+
+
+
+// const PORT = process.env.PORT || 8080;
+
+// app.listen(PORT, async () => {
+//   try {
+//     await connection;
+//     console.log("Our database is  connected");
+//     console.log(`Server is running on port ${PORT}`);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+
+
+
+
+
+const express=require("express")
+const { connection } = require("./db")
+const { userRouter } = require("./Routes/user.routes")
+const { PostRouter } = require("./Route/post.routes")
+const cors = require('cors')
+
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/users",userRouter)
+app.use("/posts",PostRouter)
+
+
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log("our database is connected");
+    } catch (error) {
+        
+    }
+   
+})
